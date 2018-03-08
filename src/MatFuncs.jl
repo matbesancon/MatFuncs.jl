3bf9f8374bca2b7f1b7ca3e56edf13daec1f5e01
mbesancon <mathieu.besancon@gmail.com>
2018-03-08 18:55:32 -0500

    yeah noew package

--- src/MatFuncs.jl
+++ src/MatFuncs.jl
@@ -2,4 +2,18 @@ module MatFuncs
 
 # package code goes here
 
+import Base: getindex, size
+
+export MatFunc
+
+struct MatFunc{F, T} <: AbstractMatrix{T} 
+    func::F
+    dims::Tuple{Int,Int}
+end
+
+size(fm::MatFunc) = fm.dims
+getindex(fm::MatFunc, i::Int, j::Int) = fm.func(i,j)
+
+MatFunc(T::DataType, func::F, dims::Tuple{Int,Int}) where {F<:Function} = MatFunc{F,T}(func,dims)
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
-using MatFuncs
+import MatFuncs
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+function ident(i,j)
+    if i == j
+        1
+    else
+        0
+    end
+end
+fm = MatFuncs.MatFunc(Int64, ident, (100,100))
+@test fm[1,1] == 1
+@test fm[1,2] == 0
+
+function identlong(i,j)
+    sleep(2)
+    if i == j
+        1
+    else
+        0
+    end
+end
+
+fm2 = MatFuncs.MatFunc(Int64, identlong, (100000,100000))
+@test fm2[1,1] == 1
+@test fm2[1,2] == 0
